ceecf8bcf2019b418001c497c1ac8e72165e99bc
Umi <98309680+umi1299@users.noreply.github.com>
2023-08-06 18:56:07 +0900

    安定版リリースをHomebrewでインストールできるようにする (#2)
    * copy something from umi1299/homebrew-voicevox
    
    * add how to install

---
+++ Casks/voicevox.rb
@@ -0,0 +1,28 @@
+cask "voicevox" do
+  version "0.14.7"
+  sha256 "341df00cced9f8b516dda4e79902d4106a34f89dc18264cb1bb6a5721282d75c"
+
+  url "https://github.com/VOICEVOX/voicevox/releases/download/#{version}/VOICEVOX.#{version}.dmg",
+      verified: "github.com/VOICEVOX/voicevox/"
+  name "VOICEVOX"
+  desc "Free, medium-quality text-to-speech software"
+  homepage "https://voicevox.hiroshiba.jp/"
+
+  livecheck do
+    url :url
+    strategy :github_latest
+  end
+
+  conflicts_with cask: "voicevox-preview"
+
+  app "VOICEVOX.app"
+
+  zap trash: [
+    "~/Library/Application Support/voicevox",
+    "~/Library/Application Support/voicevox-cpu",
+    "~/Library/Application Support/voicevox-engine",
+    "~/Library/Logs/voicevox-cpu",
+    "~/Library/Preferences/jp.hiroshiba.voicevox.plist",
+    "~/Library/Saved Application State/jp.hiroshiba.voicevox.savedState",
+  ]
+end
--- README.md
+++ README.md
@@ -1,1 +1,10 @@
+# homebrew-voicevox
+
 HomebrewでVOICEVOXをインストールできるようにするリポジトリです。
+
+## インストール方法
+
+```sh
+$ brew tap VOICEVOX/voicevox
+$ brew install --cask --no-quarantine voicevox
+```
