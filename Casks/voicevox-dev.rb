06ca4785bb8fb8769cdf7502a3e8fdfc92602bf6
Hiroshiba <hihokaruta@gmail.com>
2024-03-11 05:13:13 +0000

    voicevox-dev 0.18.0-dev

--- Casks/voicevox-dev.rb
+++ Casks/voicevox-dev.rb
@@ -1,5 +1,5 @@
 cask "voicevox-dev" do
-  version "0.17.0-dev"
+  version "0.18.0-dev"
   sha256 :no_check
 
   url "https://github.com/VOICEVOX/voicevox/releases/download/#{version}/VOICEVOX.#{version}.dmg",
