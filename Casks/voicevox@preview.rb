774d601df15fbcd2f057c85123577ff22be00311
Han Yeong-woo <han@yeongwoo.dev>
2024-06-18 04:47:16 +0900

    Version区切り文字を公式Caskと同じように`@`にする (#45)
    次のような利点が得られます:
    - 後で公式Caskに移す時、摩擦を減らすことができる。
    - ユーザーが公式Caskと同じ方式で使用できる。

--- Casks/voicevox@preview.rb
+++ Casks/voicevox@preview.rb
@@ -1,9 +1,8 @@
-cask "voicevox-dev" do
-  arch arm: "arm64", intel: "x64"
-  version "0.20.0-dev"
-  sha256 :no_check
+cask "voicevox@preview" do
+  version "0.16.0-preview.1"
+  sha256 "14b48d4a3723fd9fc211e5c350f7563b56613ab981258038c3fda302acb17f0b"
 
-  url "https://github.com/VOICEVOX/voicevox/releases/download/#{version}/VOICEVOX.#{version}-#{arch}.dmg",
+  url "https://github.com/VOICEVOX/voicevox/releases/download/#{version}/VOICEVOX.#{version}.dmg",
       verified: "github.com/VOICEVOX/voicevox/"
   name "VOICEVOX"
   desc "Free, medium-quality text-to-speech and singing synthesizer software"
@@ -11,20 +10,22 @@ cask "voicevox-dev" do
 
   livecheck do
     url :url
-    regex(/(\d+(?:\.\d+)*-dev)$/i)
+    regex(/(\d+(?:\.\d+)*-preview\.\d+)$/i)
   end
 
   conflicts_with cask: [
     "voicevox",
-    "voicevox-preview",
+    "voicevox@dev",
   ]
 
   app "VOICEVOX.app"
 
   zap trash: [
     "~/Library/Application Support/voicevox",
+    "~/Library/Application Support/voicevox-cpu",
     "~/Library/Application Support/voicevox-engine",
     "~/Library/Logs/voicevox",
+    "~/Library/Logs/voicevox-cpu",
     "~/Library/Preferences/jp.hiroshiba.voicevox.plist",
     "~/Library/Saved Application State/jp.hiroshiba.voicevox.savedState",
   ]
