f0eeea891a593ccf91688426080b3ed8eafa799d
Hiroshiba <hihokaruta@gmail.com>
2024-12-30 14:17:05 +0900

    voicevox 0.22.3 (#55)

--- Casks/voicevox.rb
+++ Casks/voicevox.rb
@@ -1,9 +1,9 @@
 cask "voicevox" do
   arch arm: "arm64", intel: "x64"
 
-  version "0.22.1"
-  sha256 arm:   "bf75e6e23450cd2c87a63d9ef217c708308ce27c3a5aefca686c0453e9607491",
-         intel: "ab87b248473bd99210546a1e062d545a2d08ef20d0b836047db91a062bbd2090"
+  version "0.22.3"
+  sha256 arm:   "7e46d125da7c90832d355778cd68f78250c46f87a68385b3012c789641e58b69",
+         intel: "f23b959934d802befaa5c5309a81d70d6993e24031f547f6993ec4f18e852d6c"
 
   url "https://github.com/VOICEVOX/voicevox/releases/download/#{version}/VOICEVOX.#{version}-#{arch}.dmg",
       verified: "github.com/VOICEVOX/voicevox/"
